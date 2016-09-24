ee0794d933d824c136df98cd289962cf0953fafd
Patrick Golden <ptgolden0@gmail.com>
2016-09-24 00:47:11 -0400

    A better animation

--- animation.js
+++ animation.js
@@ -1,64 +1,80 @@
 "use strict";
 
-var el = document.createElement('div')
+const DURATION = 200
+    , UNIT = 24
 
-el.style.overflow = 'hidden';
-el.style.position = 'absolute';
-el.style.top = 0;
-el.style.bottom = 0;
-el.style.left = 0;
-el.style.right = 0;
 
-document.body.insertBefore(el, document.body.children[0]);
-
-function makeOrg(lighten) {
-  ['O', 'R', 'G'].forEach(function (letter, i) {
-    var letterEl = document.createElement('span')
-
-    letterEl.textContent = letter;
-
-    letterEl.style.position = 'absolute';
-    letterEl.style.top = '50%';
-    letterEl.style.left = '50%';
-
-    letterEl.style.fontSize = '24px'
-    letterEl.style.color = '#76aed0'
-
-    if (lighten) {
-      letterEl.style.opacity = '.1';
-    }
-
-    letterEl.animate([
-      { transform: 'rotate(' + (360 * (i / 3)) + 'deg)' },
-      { transform: 'rotate(' + (360 * ((i + 1) / 3)) + 'deg)' },
-      { transform: 'rotate(' + (360 * ((i + 2) / 3)) + 'deg)' },
-      { transform: 'rotate(' + (360 * ((i + 3) / 3)) + 'deg)' },
-    ], {
-      duration: 5000,
-      iterations: Infinity,
-      easing: 'ease-in-out'
-    })
-
-    letterEl.animate([
-      { transformOrigin: '0vh' },
-      { transformOrigin: '-50vh' },
-      { transformOrigin: '0vh' },
-      { transformOrigin: '50vh' },
-      { transformOrigin: '0vh' }
-    ], {
-      duration: 10000,
-      iterations: Infinity
-    })
-
-    el.appendChild(letterEl);
-  });
+function trueOrFalse() {
+  return Math.random() > .5
 }
 
-var NUMBER_OF_ORGS = 18
-  , SPACING_DELAY = 10
-
-for (var i = 0; i < NUMBER_OF_ORGS; i++) {
-  var lighten = i > 0 && i < NUMBER_OF_ORGS - 1
-
-  setTimeout(makeOrg.bind(null, lighten), SPACING_DELAY * i);
+function px({ right, top }) {
+  return { right: right + 'px', top: top + 'px' }
 }
+
+
+function nextPos(prev) {
+  const direction = trueOrFalse() ? 'right' : 'top'
+      , multiplier = trueOrFalse() ? 1 : -1
+
+  const nextValue = Math.abs(prev[direction] + (UNIT * multiplier))
+
+  return Object.assign({}, prev, { [direction]: nextValue })
+}
+
+
+function* getState(length) {
+  const positions = [{ top: 0, right: 0 }]
+
+  while (positions.length < length) {
+    positions.push(nextPos(positions[positions.length - 1]))
+  }
+
+  yield positions;
+
+  while (true) {
+    positions.shift();
+    positions.push(nextPos(positions[positions.length - 1]));
+    yield positions;
+  }
+}
+
+
+function start() {
+  const container = document.createElement('div')
+      , state = getState(4)
+
+  container.style.position = 'absolute';
+  container.style.right = 0;
+  container.style.top = 0;
+
+  document.body.appendChild(container);
+
+  const letters = ['O', 'R', 'G'].map(letter => {
+    const el = document.createElement('span');
+
+    el.style.position = 'absolute';
+    el.style.right = '-999px';
+    el.style.top = '-999px';
+    el.style.fontWeight = 'bold';
+    el.style.color = '#76aed0';
+    el.style.fontSize = '20px';
+
+    el.textContent = letter;
+    container.appendChild(el);
+
+    return el;
+  }).reverse()
+
+  setInterval(() => {
+    const positions = state.next().value.map(px)
+
+    letters.forEach((el, i) => {
+      el.animate([positions[i], positions[i + 1]], DURATION)
+    })
+  }, DURATION)
+}
+
+start();
+
+setInterval(start, 666);
